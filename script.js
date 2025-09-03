883ee9cc4ceb6183ca6528c1b75593ea1f799e08
Aryan Kumar <aryankrkyn@gmail.com>
2025-09-03 05:56:13 +0530

    added UI

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function playRound (humanChoice, computerChoice) {
     if(humanChoice===computerChoice)
     {
        
-        console.log("TIE");
-        return;
+        return "TIE";
+      
     }
     if(humanChoice==="rock"){
         if(computerChoice==="scissors")
@@ -46,17 +46,78 @@ function playRound (humanChoice, computerChoice) {
         message += "Lose!. " + computerChoice + " beats " + humanChoice;
     else message += "Win!. " + humanChoice + " beats " + computerChoice;
 
-    console.log(message);
+      return message;
 
 }
 
 function playGame (){
-    for(let i=0; i<5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        console.log("round is over");
+   
+const rockBtn = document.createElement("button");
+rockBtn.setAttribute("id", "rock");
+rockBtn.textContent = "rock";
+const paperBtn = document.createElement("button");
+paperBtn.setAttribute("id", "paper");
+paperBtn.textContent = "paper";
+const scissorsBtn = document.createElement("button");
+scissorsBtn.setAttribute("id", 'scissors');
+scissorsBtn.textContent = "scissors";
+
+const buttonPanel = document.createElement("div");
+document.body.appendChild(buttonPanel);
+buttonPanel.appendChild(rockBtn);
+buttonPanel.appendChild(paperBtn);
+buttonPanel.appendChild(scissorsBtn);
+
+let playerSelection = "", computerSelection = "";
+
+const scoreBoard = document.createElement("div");
+function updateScoreBoard()
+{
+    scoreBoard.textContent = "Player: " + humanScore + " | " + "Computer: " + computerScore;
+}
+
+const roundResult = document.createElement("div");
+roundResult.textContent = "Begin Playing";
+const display = document.createElement("div");
+const winnerCard = document.createElement("div");
+winnerCard.textContent = "**********"
+display.appendChild(roundResult);
+display.appendChild(scoreBoard);
+display.appendChild(winnerCard);
+
+roundResult.style.textAlign = "center";
+display.style.textAlign = "center";
+roundResult.style.margin = "3px";
+document.body.appendChild(display);
+document.body.appendChild(buttonPanel);
+// document.body.append()
+
+buttonPanel.style.display = "flex";
+buttonPanel.style.gap = "15px";
+buttonPanel.style.justifyContent = "space-around";
+buttonPanel.style.margin = "10px";
+
+document.addEventListener("click", (e) => {
+    if(e.target.id && computerScore<5 && humanScore <5)
+    {
+        playerSelection = e.target.id==="rock"? "rock" : e.target.id==="paper"? "paper": "scissors";
+        console.log(`The player selected ${playerSelection}`);
+        computerSelection = getComputerChoice();
+        const message = playRound(playerSelection, computerSelection);
+        roundResult.textContent = message;
+        updateScoreBoard();
+        
+        if(computerScore===5){
+            winnerCard.textContent = "Computer Wins!"
+        }
+        if(humanScore===5){
+            winnerCard.textContent = "Player Wins!"
+        }
+        
     }
+});
+
+
 }
 playGame();
 
